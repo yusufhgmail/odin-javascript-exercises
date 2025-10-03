67d16e865caa4e32242fa2e7ccac17a8e1147913
yusufhgmail <yusufh@gmail.com>
2025-10-03 14:16:13 +0700

    Finish 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+	let newStr = "";
+	for (let i = string.length - 1; i >= 0; i--) {
+		newStr = newStr + string[i];
+	}
+	return newStr;
 };
+
+console.log(reverseString("helo"));
 
 // Do not edit below this line
 module.exports = reverseString;
