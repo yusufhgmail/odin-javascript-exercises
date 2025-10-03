a6e04ec8b906c5305c84b449085e66ef77e1f363
yusufhgmail <yusufh@gmail.com>
2025-10-03 15:54:07 +0700

    Finish 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...toRemove) {
+	for (let arg of toRemove) {
+		array = array.filter((arrayLookup) => arrayLookup != arg);
+	}
+	return array;
 };
+
+let array = [1, 2, 3, 4, 5];
+
+console.log(removeFromArray(array, 3, 4));
 
 // Do not edit below this line
 module.exports = removeFromArray;
