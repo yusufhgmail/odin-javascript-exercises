29eec21f2816a33901df50196bfe49bd9b8544a8
yusufhgmail <yusufh@gmail.com>
2025-10-03 13:54:01 +0700

    Finish 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+	let newString = "";
+	for (let i = 1; i <= number; i++) {
+		newString = newString + string;
+	}
+	return newString;
 };
 
 // Do not edit below this line
