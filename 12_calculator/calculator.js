9d02047b44bd0a403f69f963fec56051c0b2dd67
yusufhgmail <yusufh@gmail.com>
2025-10-08 10:53:14 +0700

    Finish 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,33 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arraySum) {
+	let sum = 0;
+	for (let i = 0; i < arraySum.length; i++) {
+		sum += arraySum[i];
+	}
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+	let sum = 1;
+	for (let i = 0; i < array.length; i++) {
+		sum *= array[i];
+	}
+	return sum;
 };
 
-const power = function() {
-	
-};
+const power = function () {};
 
-const factorial = function() {
-	
-};
+const factorial = function () {};
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
